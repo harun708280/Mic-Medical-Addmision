da4301e6ea08e9d3c1bd7d021740799ffeb064b5
harunbd80 <harun708280@gmail.com>
2024-05-20 00:34:52 +0600

    'update'

--- Mico/urls.py
+++ Mico/urls.py
@@ -5,5 +5,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Main.urls')),
+    
+    
     path('accaount/',include('Account.urls')),
 ]
